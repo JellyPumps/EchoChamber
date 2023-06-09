cea5e6e21768689a0908e4288b9aca1dc0c67a72
Jelly <sarthakrai3406@gmail.com>
2023-06-09 12:07:30 +0530

    UPDATE dialoguebox -> Outline & colour change

--- src/dialoguebox.cpp
+++ src/dialoguebox.cpp
@@ -9,38 +9,44 @@ PictureInfo picture;
 // Dialogue Properties
 const float DX_POS_X = 7.0 / 40.0;
 const float DX_POS_Y = 3.0 / 4.0;
-const float DX_SIZ_X = 2.0;
-const float DX_SIZ_Y = 6.0;
+const float DX_SIZ_X = 1.0 / 2.0;
+const float DX_SIZ_Y = 1.0 / 6.0;
 
-const float PX_POS_X = 40.0;
+const float PX_POS_X = 1.0 / 40.0;
 const float PX_POS_Y = 3.0 / 4.0;
-const float PX_SIZ_X = 8.0;
-const float PX_SIZ_Y = 6.0;
+const float PX_SIZ_X = 1.0 / 8.0;
+const float PX_SIZ_Y = 1.0 / 6.0;
+
+const float OT_POS_X = 1.0 / 80.0;
+const float OT_POS_Y = 11.0 / 15.0;
+const float OT_SIZ_X = 27.0 / 40.0;
+const float OT_SIZ_Y = 1.0 / 5.0;
 
 void DrawDialogue()
 {
+
+    // Dialogue Outline
+    DrawRectangle(
+        RESOLUTION[0] * OT_POS_X,
+        RESOLUTION[1] * OT_POS_Y,
+        RESOLUTION[0] * OT_SIZ_X,
+        RESOLUTION[1] * OT_SIZ_Y,
+        PLEARTH_ONE
+    );
+
     // Dialogue Container Rect
     DrawRectangle(
         RESOLUTION[0] * DX_POS_X,
         RESOLUTION[1] * DX_POS_Y,
-        RESOLUTION[0] / DX_SIZ_X,
-        RESOLUTION[1] / DX_SIZ_Y,
-        KEYSTONE
-    );
-
-    // Dialogue Picture Outline
-    DrawRectangle(
-        RESOLUTION[0] / PX_POS_X - 5,
-        RESOLUTION[1] * PX_POS_Y - 5,
-        RESOLUTION[0] / PX_SIZ_X + 10,
-        RESOLUTION[1] / PX_SIZ_Y + 10,
-        KEYSTONE
+        RESOLUTION[0] * DX_SIZ_X,
+        RESOLUTION[1] * DX_SIZ_Y,
+        PLEARTH_TWO
     );
 
     // Dialogue Picture
     DrawTexture(
         picture.character,
-        RESOLUTION[0] / PX_POS_X,
+        RESOLUTION[0] * PX_POS_X,
         RESOLUTION[1] * PX_POS_Y,
         WHITE
     );
