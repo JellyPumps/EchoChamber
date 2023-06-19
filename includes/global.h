f8e6284ee82980a3600035661001ec1a04a1e27a
Jelly <sarthakrai3406@gmail.com>
2023-06-19 09:37:21 +0530

    Fixed Globals

--- includes/global.h
+++ includes/global.h
@@ -1,35 +1,12 @@
 #pragma once
 
-extern int RESOLUTION[2];
+extern int g_RESOLUTION[2];
 
-extern const int FPS;
-extern const int WIDTH;
-extern const int HEIGHT;
+extern const int g_FPS;
+extern const int g_WIDTH;
+extern const int g_HEIGHT;
 
-extern const char *TITLE;
+extern const char *g_TITLE;
 
-extern const float FONT_SIZE;
-extern const float FONT_SPACING;
-
-extern const float DX_POS_X;
-extern const float DX_POS_Y;
-extern const float DX_SIZ_X;
-extern const float DX_SIZ_Y;
-
-extern const float PX_POS_X;
-extern const float PX_POS_Y;
-extern const float PX_SIZ_X;
-extern const float PX_SIZ_Y;
-
-extern const float OT_POS_X;
-extern const float OT_POS_Y;
-extern const float OT_SIZ_X;
-extern const float OT_SIZ_Y;
-
-extern const float FONT_X;
-extern const float FONT_Y;
-
-extern const float CONT_X;
-extern const float CONT_Y;
-
-extern bool waitForInput;
+extern const float g_FONT_SIZE;
+extern const float g_FONT_SPACING;
--- src/global.cpp
+++ src/global.cpp
@@ -1,35 +1,12 @@
 #include "global.h"
 
-int RESOLUTION[2] = {800,600};
+int g_RESOLUTION[2] = {800,600};
 
-const int FPS = 60;
-const int WIDTH  = RESOLUTION[0];
-const int HEIGHT = RESOLUTION[1];
+const int g_FPS     = 60;
+const int g_WIDTH   = g_RESOLUTION[0];
+const int g_HEIGHT  = g_RESOLUTION[1];
 
-const char *TITLE = "Echo Chamber";
+const char *g_TITLE   = "Echo Chamber";
 
-const float FONT_SIZE = WIDTH / 50.0;
-const float FONT_SPACING = WIDTH / 400.0;
-
-const float DX_POS_X = WIDTH * 7.0 / 40.0;
-const float DX_POS_Y = HEIGHT *3.0 / 4.0;
-const float DX_SIZ_X = WIDTH * 1.0 / 2.0;
-const float DX_SIZ_Y = HEIGHT * 1.0 / 6.0;
-
-const float PX_POS_X = WIDTH * 1.0 / 40.0;
-const float PX_POS_Y = HEIGHT * 3.0 / 4.0;
-const float PX_SIZ_X = WIDTH * 1.0 / 8.0;
-const float PX_SIZ_Y = HEIGHT * 1.0 / 6.0;
-
-const float OT_POS_X = WIDTH * 1.0 / 80.0;
-const float OT_POS_Y = HEIGHT * 11.0 / 15.0;
-const float OT_SIZ_X = WIDTH * 27.0 / 40.0;
-const float OT_SIZ_Y = HEIGHT * 1.0 / 5.0;
-
-const float FONT_X = DX_POS_X + FONT_SIZE;
-const float FONT_Y = DX_POS_Y + FONT_SPACING;
-
-const float CONT_X = 19.0 * FONT_SIZE * FONT_SPACING;
-const float CONT_Y = HEIGHT - FONT_SIZE;
-
-bool waitForInput = true;
+const float g_FONT_SIZE     = g_WIDTH / 50.0;
+const float g_FONT_SPACING  = g_WIDTH / 400.0;
