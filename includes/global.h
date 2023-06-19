79beb22f12983a156fcb6970acacc1632182d009
Jelly <sarthakrai3406@gmail.com>
2023-06-19 09:47:18 +0530

    globals is disappearing D:

--- includes/global.h
+++ includes/global.h
@@ -3,8 +3,6 @@
 extern int g_RESOLUTION[2];
 
 extern const int g_FPS;
-extern const int g_WIDTH;
-extern const int g_HEIGHT;
 
 extern const char *g_TITLE;
 
--- src/global.cpp
+++ src/global.cpp
@@ -3,10 +3,8 @@
 int g_RESOLUTION[2] = {800,600};
 
 const int g_FPS     = 60;
-const int g_WIDTH   = g_RESOLUTION[0];
-const int g_HEIGHT  = g_RESOLUTION[1];
 
 const char *g_TITLE   = "Echo Chamber";
 
-const float g_FONT_SIZE     = g_WIDTH / 50.0;
-const float g_FONT_SPACING  = g_WIDTH / 400.0;
+const float g_FONT_SIZE     = g_RESOLUTION[0] / 50.0;
+const float g_FONT_SPACING  = g_RESOLUTION[0] / 400.0;
